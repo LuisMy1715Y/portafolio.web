db884a6c41b69412241b03248b8e5f1a74c84922
Luis Miguel Hernandez <hernandezsuarezluismiguel@gmail.com>
2025-03-07 15:30:28 -0500

    Fix Astro config for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://LuisMy1715Y.github.io',
-  base: 'porfolio.web',
+  output: "static", // Esto es clave para Vercel
 });
-
