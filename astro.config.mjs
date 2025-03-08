6f2f559bb023ca529996c46e38a0bc4d28251435
Luis Miguel Hernandez <hernandezsuarezluismiguel@gmail.com>
2025-03-07 21:27:04 -0500

    Actualization for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static", // Esto es clave para Vercel
+
+  // Esto es clave para Vercel
+  output: "static",
+
+  adapter: vercel()
 });
